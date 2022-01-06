ec983400cbfe0a7e132cd27e351732dec549d7d2
Melissa Hiatt <melissahiatt@gmail.com>
2022-01-06 10:58:50 -0800

    Setup Google PDF Block

--- blocks/google-embed-pdf/build/index.js
+++ blocks/google-embed-pdf/build/index.js
@@ -97,7 +97,7 @@ __webpack_require__.r(__webpack_exports__);
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
  */
 
-(0,_wordpress_blocks__WEBPACK_IMPORTED_MODULE_0__.registerBlockType)('create-block/fim-blocks', {
+(0,_wordpress_blocks__WEBPACK_IMPORTED_MODULE_0__.registerBlockType)('fim-blocks/google-embed-pdf', {
   /**
    * @see ./edit.js
    */
--- blocks/google-embed-pdf/src/index.js
+++ blocks/google-embed-pdf/src/index.js
@@ -25,7 +25,7 @@ import save from './save';
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
  */
-registerBlockType('create-block/fim-blocks', {
+registerBlockType('fim-blocks/google-embed-pdf', {
 	/**
 	 * @see ./edit.js
 	 */
