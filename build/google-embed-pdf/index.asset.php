7fb9b330c001ec1ac7358be430328b6722a8c7bb
Melissa Hiatt <melissahiatt@gmail.com>
2022-12-09 12:02:55 -0800

    rebuild css

--- build/google-embed-pdf/index.asset.php
+++ build/google-embed-pdf/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '97391d5cb9b858fe10d4');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '21211d19e23589daa51a');
