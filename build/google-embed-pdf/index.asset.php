0174f01e57351c762882eaa0c2a7500e3e1dd79f
Melissa Hiatt <melissahiatt@gmail.com>
2025-12-12 21:15:22 -0800

    build file

--- build/google-embed-pdf/index.asset.php
+++ build/google-embed-pdf/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'eb2ab9104db3d01e7f3d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '2fca3affa6184f99712f');
--- build/mobile-buttons/index.asset.php
+++ build/mobile-buttons/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'de3a887bbe8f9e532644');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5f204f496173f2ec7cef');
