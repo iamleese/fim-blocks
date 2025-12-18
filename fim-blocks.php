10f1135e2068e1ad81b6f1a60e6167c9fa088f6d
Melissa Hiatt <melissahiatt@gmail.com>
2025-12-17 21:20:04 -0800

    Versioning

--- fim-blocks.php
+++ fim-blocks.php
@@ -4,7 +4,7 @@
  * Description:       Custom blocks built by Faith in Marketing. Contains Facebook Album Embed, Google Drive PDF Embed, and Mobile Buttons.
  * Requires at least: 6.0
  * Requires PHP:      7.1
- * Version:           2.1.3
+ * Version:           2.1.4
  * Author:            Melissa Hiatt, Faith in Marketing
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
